372cd6ff670c4d78a82bf5ef2d77077e7e23e603
hugom <furiousfort@gmail.com>
2026-02-06 01:21:29 +0100

    add : profile last tweet

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TweetsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,19 @@ class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
     #[IsGranted('ROLE_USER')]
-    public function myProfile(): Response
+    public function myProfile(TweetsRepository $tweetsRepository): Response
     {
+        $user = $this->getUser();
+
+
+        $tweets = $tweetsRepository->findBy(
+            ['author' => $user],
+            ['createdAt' => 'DESC']
+        );
+
         return $this->render('profile/my_profile.html.twig', [
-            'user' => $this->getUser(),
+            'user' => $user,
+            'tweets' => $tweets,
         ]);
     }
 }
-
-
